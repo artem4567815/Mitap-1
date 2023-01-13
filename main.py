1c375f26d90d1a21868f195fc5327c6ca7523295
Артём <artemiy_08@icloud.com>
2023-01-13 23:07:50 +0300

    mitap4

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from tkinter import *
 import random
 from PIL import Image, ImageTk
-
 tk = Tk()
 tk.geometry("1280x1024")
-tk.attributes('-fullscreen', True)
 c = Canvas(tk, width=1280, height=1024, bg="white")
 c.pack()
 tk["bg"] = "black"
 
 N = 9
 vy = 136
+
+res = []
+
 color = {"red": ImageTk.PhotoImage(Image.open("images/red.png").resize((136,136), Image.ANTIALIAS)),
          "orange": ImageTk.PhotoImage(Image.open("images/orange.png").resize((136,136), Image.ANTIALIAS)),
          "blue": ImageTk.PhotoImage(Image.open("images/blue.png").resize((136,136), Image.ANTIALIAS)),
          "green": ImageTk.PhotoImage(Image.open("images/green.png").resize((136,136), Image.ANTIALIAS))}
 
-res = []
-background = ImageTk.PhotoImage(Image.open("images/backgr.png").resize((1280, 1024), Image.ANTIALIAS))
+background = ImageTk.PhotoImage(Image.open("images/backr.png").resize((1280, 1024), Image.ANTIALIAS))
+bac = c.create_image(0, 0, image=background, anchor=NW)
+
 cannon_creat_green = ImageTk.PhotoImage(Image.open("images/cannon.png").resize((144,168), Image.ANTIALIAS))
 cannon_creat_blue = ImageTk.PhotoImage(Image.open("images/blue_cannon.png").resize((324,168), Image.ANTIALIAS))
 
@@ -26,28 +28,21 @@ bullet_creat_blue = ImageTk.PhotoImage(Image.open("images/bullet_blue.png").resi
 
 cannon_green = c.create_image(170, 669, image=cannon_creat_green, anchor=NW)
 cannon_blue = c.create_image(360, 669, image=cannon_creat_blue, anchor=NW)
-
-bac = c.create_image(0, 0, image=background, anchor=NW, tag="DOWN")
-c.tag_lower("DOWN")
-
+c.tag_raise(cannon_green)
 bullet = []
 bullet_blue = []
-
-
-
 def spawn_squ():
     x = 30
     for _ in range(N):
         cc = random.choice(list(color))
-        res1 = c.create_image(x, -136, image=color[cc], anchor=NW, tag="down")
-        #c.tag_lower("down")
+        res1 = c.create_image(x, -136, image=color[cc], anchor=NW)
         res.append({"color": cc, "img": res1})
         x += 136
     c.after(5000, spawn_squ)
 c.after(5000, spawn_squ)
 
 def Move_squ():
-    global vy, x_mouse, y_mouse
+    global vy
     for i in range(len(res)):
         res1 = res[i]["img"]
         c.move(res1, 0, vy)
@@ -57,19 +52,20 @@ c.after(5000, Move_squ)
 def spawn_bullet_green(event):
     for i in range(len(res)):
         res1 = res[i]["img"]
-        #c.tag_lower("dow")
         x, y = c.coords(res1)
         if event.x > x and event.x <= x + 136 and event.y >= y and event.y <= y + 136:
-            bullet1 = c.create_image(202, 669, image=bullet_creat_green, anchor=NW, tag="dow")
+            bullet1 = c.create_image(202, 669, image=bullet_creat_green, anchor=NW)
+            c.tag_lower(bullet1)
             bullet.append({"fill": bullet1})
-#c.bind("<Button-1>", spawn_bullet_green)
+
 
 def spawn_bullet_blue(event):
     for i in range(len(res)):
         res1 = res[i]["img"]
         x, y = c.coords(res1)
         if event.x > x and event.x <= x + 136 and event.y >= y and event.y <= y + 136:
-            bullet2= c.create_image(496, 669, image=bullet_creat_blue, anchor=NW, tag="dowj")
+            bullet2 = c.create_image(496, 669, image=bullet_creat_blue, anchor=NW)
+            c.tag_lower(bullet2)
             bullet_blue.append({"fill": bullet2})
 
 def Move_two_bul(event):
@@ -84,12 +80,28 @@ def Move_bullet():
 c.after(50, Move_bullet)
 
 def Move_bullet_blue():
+    c.tag_raise(cannon_blue)
+    c.tag_raise(cannon_green)
     for i in range(len(bullet_blue)):
         bullet1 = bullet_blue[i]["fill"]
         c.move(bullet1, 0, -2)
     c.after(50, Move_bullet_blue)
 c.after(50, Move_bullet_blue)
 
+def Layer():
+    for i in range(len(bullet)):
+        bullet1 = bullet[i]["fill"]
+        x, y = c.coords(bullet1)
+        if y < 660:
+            c.tag_raise(bullet1)
+    for j in range(len(bullet_blue)):
+        bullet2 = bullet_blue[j]["fill"]
+        x1, y1 = c.coords(bullet2)
+        if y1 < 600:
+            c.tag_raise(bullet2)
+
+    c.after(50, Layer)
+c.after(50, Layer)
 def Player():
     n = 0
     for j in range(len(bullet)):
